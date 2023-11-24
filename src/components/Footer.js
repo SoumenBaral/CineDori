45e84a26c1254472317483f713f5c74876252920
SoumenBaral <shumen.sb@gmail.com>
2023-11-24 21:11:43 +0600

    header and footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-         <footer className="bg-white rounded-lg shadow  dark:bg-gray-800">
+         <footer className="bg-white  shadow  dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">CineDore™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to={"/"} className="hover:underline">CineDore™</Link>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="#" target={'_blank'} className="hover:underline me-4 md:me-6">About</a>
+                <a href="#" target={'_blank'} className="hover:underline me-4 md:me-6">Github</a>
             </li>
             <li>
-                <a href="#" target={'_blank'}  className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                <a href="#" target={'_blank'}  className="hover:underline me-4 md:me-6">FaceBook</a>
             </li>
             <li>
-                <a href="#" target={'_blank'}  className="hover:underline me-4 md:me-6">Licensing</a>
+                <a href="#" target={'_blank'}  className="hover:underline me-4 md:me-6">Linkdin</a>
             </li>
             <li>
-                <a href="#" target={'_blank'}  className="hover:underline">Contact</a>
+                <a href="#" target={'_blank'}  className="hover:underline">tweeter</a>
             </li>
         </ul>
         </div>
