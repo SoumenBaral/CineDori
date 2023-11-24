10c9f37fad82a4689f6a38483afebc4f1d3ba099
SoumenBaral <shumen.sb@gmail.com>
2023-11-24 11:36:20 +0600

    just arrange all the commopents and pages add delete some unwanted things

---
+++ src/Pages/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieList = () => {
+    return (
+        <div>
+            <h1>Movies</h1>
+        </div>
+    );
+};
+
+export default MovieList;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>header 2030</p>
+        </div>
+    );
+};
+
+export default Footer;
