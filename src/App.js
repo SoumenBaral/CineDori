320279cd6401a0f7df1b614bc90da0780ba4d3e2
SoumenBaral <shumen.sb@gmail.com>
2023-11-24 20:49:11 +0600

    add header and footer form the flowbite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import AllRouts from "./routes/AllRouts";
 
 
 function App() {
   return (
     <div className="App">
-     <AllRouts></AllRouts>
+      <Header/>
+     <AllRouts/>
+     <Footer />
     </div>
   );
 }
