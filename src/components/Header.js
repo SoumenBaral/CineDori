74f2c42a09ed478c56009284a18dacdcc3f11258
SoumenBaral <shumen.sb@gmail.com>
2023-12-10 19:17:02 +0600

    add card in movieList"

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
 
     return (
         <header>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-white border-b-2 dark:border-b-2 dark:border-gray-900 border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     
                     <NavLink to={"/"} className="flex items-center space-x-3 rtl:space-x-reverse">
